fe397f6c296e596c6740dc6f17b0d9436a68341f
Artem Baguinski <abaguinski@gmail.com>
2023-07-30 19:32:26 +0200

    Pretend to choose the best move
    Overcomplicate the stupid bot by pretending to evaluate the available
    options

--- Chess-Challenge/src/My Bot/MyBot.cs
+++ Chess-Challenge/src/My Bot/MyBot.cs
@@ -1,10 +1,38 @@
-﻿using ChessChallenge.API;
+using ChessChallenge.API;
+using System.Collections.Generic;
+using System.Linq;
 
 public class MyBot : IChessBot
 {
+    class Option {
+        public Move move;
+        public int score;
+
+        public Option(Move move) {
+            this.move = move;
+            score = 0;
+        }
+    }
+
     public Move Think(Board board, Timer timer)
     {
-        Move[] moves = board.GetLegalMoves();
-        return moves[0];
+        var ahead = SeeAhead(board);
+        return ahead.MaxBy(option => option.score).move;
+    }
+
+    Option[] SeeAhead(Board board) {
+        var moves = board.GetLegalMoves();
+        var options = moves.Select(move => new Option(move)).ToArray();
+        foreach (Option option in options) {
+            board.MakeMove(option.move);
+            option.score = - Evaluate(board);
+            board.UndoMove(option.move);
+        }
+        return options;
+    }
+
+    // evaluate the board from point of view of whose move it is
+    int Evaluate(Board board) {
+        return 0;
     }
 }
