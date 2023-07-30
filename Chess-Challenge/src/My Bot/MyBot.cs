146029c4cdbda855864b3a064ac9fb57b990d665
Artem Baguinski <abaguinski@gmail.com>
2023-07-30 19:32:27 +0200

    Be better than the Evil Bot
    Looking 1 ply further than the Evil Bot is enough to beat them more
    often than be beaten. Also inverted the meaning of "Evaluate" since it's
    used to evaluate a move that arrived there.

--- Chess-Challenge/src/My Bot/MyBot.cs
+++ Chess-Challenge/src/My Bot/MyBot.cs
@@ -18,26 +18,36 @@ public class MyBot : IChessBot
 
     public Move Think(Board board, Timer timer)
     {
-        var ahead = SeeAhead(board);
+        var ahead = SeeAhead(board, 2);
         return ahead.MaxBy(option => option.score).move;
     }
 
-    Option[] SeeAhead(Board board) {
+    Option[] SeeAhead(Board board, int depth) {
         var moves = board.GetLegalMoves();
         var options = moves.Select(move => new Option(move)).ToArray();
         foreach (Option option in options) {
             board.MakeMove(option.move);
-            option.score = - Evaluate(board);
+            if (depth == 1) {
+                option.score = Evaluate(board);
+            } else {
+                var theirOptions = SeeAhead(board, depth - 1);
+                if (theirOptions.Length > 0) {
+                    // score for us is the inverse of their best move
+                    option.score = - theirOptions.Select(o => o.score).Max();
+                } else {
+                    option.score = Evaluate(board);
+                }
+            }
             board.UndoMove(option.move);
         }
         return options;
     }
 
-    // evaluate the board from point of view of whose move it is
+    // evaluate the board from our point of view when it is their move
     int Evaluate(Board board) {
-        if (board.IsInCheckmate()) return -1000;
+        if (board.IsInCheckmate()) return 1000;
 
-        bool us = board.IsWhiteToMove, them = !us;
+        bool them = board.IsWhiteToMove, us = !them;
         int score = 0;
         for(PieceType type = PieceType.Pawn; type < PieceType.King; type++) {
             int balance = board.GetPieceList(type, us).Count() - board.GetPieceList(type, them).Count();
