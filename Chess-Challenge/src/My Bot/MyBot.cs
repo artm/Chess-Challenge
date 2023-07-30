8740ac9ee1732efdef9892477a413873648dd617
Artem Baguinski <abaguinski@gmail.com>
2023-07-30 19:32:27 +0200

    Implement alpha-beta search without move ordering

--- Chess-Challenge/src/My Bot/MyBot.cs
+++ Chess-Challenge/src/My Bot/MyBot.cs
@@ -3,18 +3,24 @@ using System;
 
 public class MyBot : IChessBot
 {
+    int MaxThinkTime = 700;
+    static int[] PieceValue = {0, 100, 300, 300, 500, 900, 10000 };
+    static int Infinity = 1000000000;
+
+    Timer timer;
+    Board board;
 
     public Move Think(Board board, Timer timer)
     {
-        return Move.NullMove;
+        this.timer = timer;
+        this.board = board;
+        return SearchRoot();
     }
 
-    static int[] PieceValue = {0, 100, 300, 300, 500, 900, 10000 };
+    int Evaluate() {
+        if (board.IsInCheckmate()) return -Infinity;
 
-    static int EvaluateBoard(Board board) {
-        if (board.IsInCheckmate()) return 100;
-
-        bool them = board.IsWhiteToMove, us = !them;
+        bool us = board.IsWhiteToMove, them = !us;
         int Score = 0;
         for(PieceType type = PieceType.Pawn; type < PieceType.King; type++) {
             int balance = board.GetPieceList(type, us).Count - board.GetPieceList(type, them).Count;
@@ -22,5 +28,43 @@ public class MyBot : IChessBot
         }
 
         return Score;
+    }
+
+    bool MayThink() {
+        return timer.MillisecondsElapsedThisTurn < MaxThinkTime;
+    }
+
+    Move SearchRoot() {
+        int bestScore = -Infinity;
+        Move bestMove = Move.NullMove;
+
+        var depth = 2;
+        while(MayThink()) {
+            depth++;
+            foreach(var move in board.GetLegalMoves()) {
+                board.MakeMove(move);
+                var score = - Search(-Infinity, Infinity, depth);
+                board.UndoMove(move);
+                if (score > bestScore) {
+                    bestScore = score;
+                    bestMove = move;
+                }
+                if (!MayThink()) break;
+            }
+        }
+        Console.WriteLine("{0,10} @ {1} in {2}ms", bestScore, depth, timer.MillisecondsElapsedThisTurn);
+        return bestMove;
+    }
+
+    int Search(int alpha, int beta, int depth) {
+        if (depth == 0 || !MayThink()) return Evaluate();
+        foreach(var move in board.GetLegalMoves()) {
+            board.MakeMove(move);
+            var score = - Search( -beta, -alpha, depth - 1);
+            board.UndoMove(move);
+            if (score >= beta) return beta;
+            if (score > alpha) alpha = score;
+        }
+        return alpha;
     }
 }
